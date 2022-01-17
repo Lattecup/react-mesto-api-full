8da6dac13a2a2cdc5917fc9b557eebd63e076d61
Anastasia Shchipakina <avshchipakina@yandex.ru>
2022-01-18 00:22:13 +0300

    fix: users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -118,10 +118,11 @@ module.exports.updateUserAvatar = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
+  const { NODE_ENV, JWT_SECRET } = process.env;
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch((err) => {
