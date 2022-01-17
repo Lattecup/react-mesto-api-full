8bfbc266ba0b361140c72fe8d7887939666dd5e5
Anastasia Shchipakina <avshchipakina@yandex.ru>
2022-01-17 15:41:40 +0300

    feat: add cors

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
 const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -17,6 +18,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(cors);
 
 app.use(requestLogger);
 
