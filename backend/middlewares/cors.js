339da895d8ad77237e96427986cfe0c762c73745
Anastasia Shchipakina <avshchipakina@yandex.ru>
2022-01-25 23:32:10 +0300

    fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
 
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
