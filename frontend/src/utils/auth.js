2508f358087c290cfc06e21b39f0457c6149bff1
Anastasia Shchipakina <avshchipakina@yandex.ru>
2022-01-26 20:18:45 +0300

    fix

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -35,7 +35,6 @@ export const checkToken = () => {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Authorization : `Bearer ${token}`,
     },
   })
   .then(handleResponse);
